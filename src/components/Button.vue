71fb70857c9ac58dd4e211fa2e4b8d27f6a040ed
Vincenzo- <viulenzgg@gmail.com>
2022-06-29 11:47:19 +0200

    vue app tutorial

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<!-- This is basically a blue print of a Vue component. We define here the different "Props" that we pass later as HTML attributes whenever we are using this component -->
+
+<template>
+  <!-- There are 2 ways to add click Events to a component. 
+1. @click="function()"
+2. v-on:click="function()"
+
+We then need to add the method/function in the export as an object
+
+-->
+  <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("Click");
+    },
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <!-- We can pass "Props" as attributes. text and color are Props, we need to define them in the component itself -->
+    <Button text="Add Task 1" color="teal" />
+    <!-- <Button text="Add Task 2" color="#5784BA" /> -->
+    <!-- <Button text="Add Task 3" color="lime" /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  // props: {                     // We can define Props as Object or as an Array
+  //   title: String,
+  // },
+  props: ["title"],
+  components: { Button },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 20px;
+}
+</style>
